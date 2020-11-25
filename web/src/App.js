c57e7e7913856e8e57da20734c787b54d1085b2d
Joao Vitor <vitor.lira98@gmail.com>
2020-11-25 11:23:38 -0300

    Finalizando web

---
+++ web/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import GlobalCss from "./global.css";
+
+import Routes from "./routes";
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
